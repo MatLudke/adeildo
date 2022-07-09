f89fa84c8713cd22c16c6ff623b0517ae6f79c72
MatLudke <mateus1ludke@gmail.com>
2022-07-09 07:16:19 -0300

    hello

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-
-const  but = document.getElementById('accept')
-const  login = document.getElementById('login')
+const but = document.getElementById('accept')
+const login = document.getElementById('login')
 const foto = document.getElementById('foto')
+const itsme = document.getElementById('itsme')
+const autentic = document.getElementById('auth')
+const itsnot = document.getElementById('itsnot')
+const father = document.getElementById('father')
+const sobre = document.getElementById('sobre')
+const confirmation = document.getElementById('confirmation')
 
 but.onclick = function myFun() {
   login.style.display = 'block'
   foto.style.display = 'none'
 }
 
-console.log("Hello World!");
+itsme.addEventListener('click', () =>{
+  autentic.style.display = 'block'
+  itsme.style.display = 'none'
+  itsnot.style.display = 'none'
+  sobre.style.display = 'none'
+  father.style.display = 'none'
+  confirmation.style.display = 'none'
+  
+})
